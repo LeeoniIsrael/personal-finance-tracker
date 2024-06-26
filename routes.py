49698cc2aa055e33e428a5e8e7d2be4580210763
Leeon Israel (Leo) <leeoniisrael@gmail.com>
2024-06-25 22:02:34 -0500

    Add API Endpoints

--- routes.py
+++ routes.py
@@ -1,6 +1,31 @@
 from flask import request, jsonify, abort
 from app import app, db
 from models import User, Transaction
+import requests
+
+API_KEY = 'your_exchange_rate_api_key'
+API_URL = 'https://v6.exchangerate-api.com/v6/{}/latest/USD'.format(API_KEY)
+
+@app.route('/convert', methods=['GET'])
+def convert_currency():
+    amount = float(request.args.get('amount'))
+    from_currency = request.args.get('from')
+    to_currency = request.args.get('to')
+
+    response = requests.get(API_URL)
+    data = response.json()
+
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to fetch exchange rates'}), 500
+
+    if from_currency not in data['conversion_rates'] or to_currency not in data['conversion_rates']:
+        return jsonify({'error': 'Invalid currency code'}), 400
+
+    rate = data['conversion_rates'][to_currency] / data['conversion_rates'][from_currency]
+    converted_amount = amount * rate
+
+    return jsonify({'amount': converted_amount}), 200
+
 
 @app.route('/users', methods=['POST'])
 def add_user():
@@ -38,3 +63,15 @@ def delete_transaction(id):
     db.session.delete(transaction)
     db.session.commit()
     return '', 204
+
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify({'username': user.username, 'email': user.email})
+
+@app.route('/users/<int:id>/transactions', methods=['GET'])
+def get_user_transactions(id):
+    user = User.query.get_or_404(id)
+    transactions = Transaction.query.filter_by(user_id=id).all()
+    return jsonify([trans.to_dict() for trans in transactions])
+
