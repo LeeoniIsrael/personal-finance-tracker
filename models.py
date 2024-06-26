5680bee8b3615768437a3524dd448a872084a1c2
haile-mena <mhailemariam@gwmail.gwu.edu>
2024-06-26 09:01:09 -0400

    added model

--- models.py
+++ models.py
@@ -1,3 +1,22 @@
 from app import db
 
 # Define the User and Transaction models here
+
+# User Model
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
+
+# Transaction Model
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric, unique=True, nullable=False)
+    category = db.Column(db.String, unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
