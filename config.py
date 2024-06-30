9efb870468a08631e6457ce29b06cdf763a191de
Leeon Israel (Leo) <leeoniisrael@gmail.com>
2024-06-29 21:00:39 -0500

    added styling

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    EXCHANGE_RATE_API_KEY = os.getenv('EXCHANGE_RATE_API_KEY', '13d3691384372bd201f6d63f45122c55')
+    EXCHANGE_RATE_API_KEY = os.getenv('EXCHANGE_RATE_API_KEY')
